53bd553c5106c1a6e383b323ef4fbede1324ad83
Jan Spindler <jspindle@uni-bonn.de>
2026-02-25 14:48:13 +0100

    added multi stage setup from README as seperate .yaml configs

---
+++ julian_scripts/train_subject00.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python main.py -c configs/subject00_julian_stage1.yaml -m train
+python main.py -c configs/subject00_julian_stage1.yaml -m render_depth_sequences
+python main.py -c configs/subject00_julian_stage2.yaml -m train
