3bf1c220103bbfb12b9986be5631311ee818a94a
Jan Spindler <jspindle@uni-bonn.de>
2026-02-27 15:25:31 +0100

    added thuman raw to gitignore + added igl install

--- install_ub.sh
+++ install_ub.sh
@@ -2,7 +2,7 @@
 
 conda install nvidia::cuda-toolkit==12.8.1 -y
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
-pip install glfw igl numpy open3d opencv-python PyOpenGL PyOpenGL-accelerate PyYAML scikit-image trimesh
+pip install glfw libigl numpy open3d opencv-python PyOpenGL PyOpenGL-accelerate PyYAML scikit-image trimesh
 export TORCH_CUDA_ARCH_LIST="12.0"
 pip install --no-build-isolation "git+https://github.com/facebookresearch/pytorch3d.git"
 
